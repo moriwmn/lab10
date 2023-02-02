1df47b87bf4a7eb8bc295b20fd222bca731d022c
moriwmn <moriya66666@gmail.com>
2023-02-02 23:12:11 +0200

    LRU and LFU implementation

--- src/test/LFU.java
+++ src/test/LFU.java
@@ -1,6 +1,40 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-public class LFU {
+
+public class LFU implements CacheReplacementPolicy{
+
+    private Map<String, Integer> frequency;
+    private Map<String, Integer> cache;
+    private PriorityQueue<String> heap;
+
+    public LFU() {
+        frequency = new HashMap<>();
+        cache = new HashMap<>();
+        heap = new PriorityQueue<>((a, b) -> frequency.get(a) - frequency.get(b));
+    }
+
+    @Override
+    public void add(String word) {
+        if (cache.containsKey(word)) {
+            int count = frequency.get(word);
+            frequency.put(word, count + 1);
+            heap.remove(word);
+            heap.offer(word);
+        } else {
+            cache.put(word, 1);
+            frequency.put(word, 1);
+            heap.offer(word);
+        }
+    }
+
+    @Override
+    public String remove() {
+        return heap.poll();
+    }
+   
 
 }
--- src/test/LRU.java
+++ src/test/LRU.java
@@ -1,6 +1,78 @@
 package test;
 
+import java.util.HashMap;
 
-public class LRU {
+public class LRU implements CacheReplacementPolicy{
+
+    class Node {
+        String key;
+        String value;
+        Node prev;
+        Node next;
+        
+        public Node(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    HashMap<String, Node> map;
+    Node head;
+    Node tail;
+    int capacity;
+
+    public LRU() {
+        map = new HashMap<>();
+        head = null;
+        tail = null;
+    }
+
+    public void add(String word) {
+        if (map.containsKey(word)) {
+            Node node = map.get(word);
+            removeNode(node);
+            setHead(node);
+        } else {
+            Node node = new Node(word, word);
+            setHead(node);
+            map.put(word, node);
+        }
+    }
+
+    public String remove() {
+        if (head == null) {
+            return null;
+        }
+        Node node = tail;
+        removeNode(node);
+        map.remove(node.key);
+        return node.value;
+    }
+
+    private void removeNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+    }
+
+    private void setHead(Node node) {
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+    }
+
 
 }
